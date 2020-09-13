295b169eca9398602ab488b776cfe18f37554192
Prathik <prathik.m@tala.co>
2020-09-13 21:05:21 +0530

    Gracefully exit on completion

--- src/main/java/prathik/Main.java
+++ src/main/java/prathik/Main.java
@@ -3,18 +3,20 @@ package prathik;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException {
     AudioPlayer audioPlayer = new AudioPlayer(Paths.get("src/main/resources/output.wav"));
+    ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
-    var future =
-        Executors.newScheduledThreadPool(1)
-            .scheduleWithFixedDelay(audioPlayer::play, 0, 3, TimeUnit.SECONDS);
+    var future = executorService.scheduleWithFixedDelay(audioPlayer::play, 0, 3, TimeUnit.SECONDS);
 
-    Thread.sleep(Duration.ofMinutes(5).toMillis());
+    Thread.sleep(Duration.ofMinutes(5).toSeconds());
     future.cancel(true);
+    executorService.shutdownNow();
+    executorService.awaitTermination(1, TimeUnit.SECONDS);
   }
 }
